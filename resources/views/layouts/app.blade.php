616934c58c2e5a7a4b199a83bb65f0a3045d5b43
feelpix <feelpix@neuf.fr>
2015-11-08 11:54:32 +0100

    bootstrap init

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>HomeCost</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"
+          type="text/css">
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+
+    <style>
+        body {
+            font-family: 'Raleway';
+            margin-top: 25px;
+        }
+    </style>
+</head>
+<body>
+    @yield('content')
+
+    <script src="/js/jquery-2.1.4.min.js"></script>
+    <script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
